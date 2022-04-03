af9acd0b05c5fa4056cb751b15756c3134bc4075
Petr Urbánek <514122@mail.muni.cz>
2022-04-03 16:23:31 +0200

    made with should

--- src/test/java/org/selenide/examples/AppTest.java
+++ src/test/java/org/selenide/examples/AppTest.java
@@ -2,8 +2,7 @@ package org.selenide.examples;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.Assert.assertEquals;
-
+import com.codeborne.selenide.CollectionCondition;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
@@ -25,7 +24,7 @@ public class AppTest
     {
         open("http://google.com");
         $(By.name("q")).setValue("Mojmir Kubistel").pressEnter();
-        assertEquals($$(withText("Mojmír Kubistel - IMDb")).size(), 1);
+        $$(withText("Mojmír Kubistel - IMDb")).should(CollectionCondition.size(1));
         $(withText("Mojmír Kubistel - IMDb")).click();
     }
 }
